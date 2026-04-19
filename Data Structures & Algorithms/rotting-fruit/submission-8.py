20cacd86126d646905fbd4a3e0ec5216230752b6
DaveMyth <55822226+DaveMyth@users.noreply.github.com>
2026-04-20 02:04:16 +0530

    Add: rotting-fruit - submission-8

---
+++ Data Structures & Algorithms/rotting-fruit/submission-8.py
@@ -0,0 +1,38 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        fruitCtr = 0
+        q = deque()
+        lvlZero = []
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = [[False]*COLS for ctr in range(ROWS)]
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    fruitCtr += 1
+                if grid[i][j] == 2:
+                    lvlZero.append((i,j))
+                    fruitCtr += 1
+                    visited[i][j] = True
+                if grid[i][j] == 0:
+                    visited[i][j] = True
+        q.append(lvlZero)
+
+        time = 0
+        moves = [(-1,0), (1,0), (0,-1), (0,1)]
+        while q:
+            currLevel = q.popleft()
+            nextLvl = []
+            for step in currLevel:
+                fruitCtr -= 1
+                for move in moves:
+                    i = step[0] + move[0]
+                    j = step[1] + move[1]
+
+                    if i>-1 and i<ROWS and j>-1 and j<COLS and not visited[i][j]:
+                        visited[i][j] = True
+                        nextLvl.append((i,j))
+            time += 1
+            if nextLvl:
+                q.append(nextLvl)
+        return -1 if fruitCtr > 0 else time-1
